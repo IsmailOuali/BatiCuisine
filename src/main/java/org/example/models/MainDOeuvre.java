9d7604e33d4caa398f3ad6bf3f142332861ca814
unknown <ismail.ouali@ump.ac.ma>
2024-09-21 18:12:17 +0100

    adding the model of mainDOeuvre

---
+++ src/main/java/org/example/models/MainDOeuvre.java
@@ -0,0 +1,37 @@
+package org.example.models;
+
+public class MainDOeuvre extends Composant{
+
+    private double tauxHoraire;
+    private double heursTravail;
+    private double productiviteOuvrier;
+    public MainDOeuvre(int id, String nom, double tauxTVA, double tauxHoraire, double heursTravail, double productiviteOuvrier) {
+        super(id, nom, "MainDOeuvre", tauxTVA);
+        this.tauxHoraire = tauxHoraire;
+        this.heursTravail = heursTravail;
+        this.productiviteOuvrier = productiviteOuvrier;
+    }
+
+    public double getTauxHoraire() {
+        return tauxHoraire;
+    }
+    public void setTauxHoraire(double tauxHoraire) {
+        this.tauxHoraire = tauxHoraire;
+    }
+
+    public double getHeursTravail() {
+        return heursTravail;
+    }
+    public void setHeursTravail(double heursTravail) {
+        this.heursTravail = heursTravail;
+    }
+
+    public double getProductiviteOuvrier() {
+        return productiviteOuvrier;
+    }
+    public void setProductiviteOuvrier(double productiviteOuvrier) {
+        this.productiviteOuvrier = productiviteOuvrier;
+    }
+
+
+}
