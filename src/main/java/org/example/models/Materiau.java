705676709dfc889df2182ed83635692467eb4f4c
unknown <ismail.ouali@ump.ac.ma>
2024-09-21 18:19:55 +0100

    Fixing model

---
+++ src/main/java/org/example/models/Materiau.java
@@ -0,0 +1,47 @@
+package org.example.models;
+
+public class Materiau extends Composant{
+
+    private double coutUnitaire;
+    private double quantite;
+    private double coutTransport;
+    private double coefficientQualite;
+
+    public Materiau(int id, String nom, double tauxTVA , double coutUnitaire, double quantite, double coefficientQualite) {
+        super(id, nom, "Material", tauxTVA);
+        this.coutUnitaire = coutUnitaire;
+        this.quantite = quantite;
+        this.coutTransport = coefficientQualite;
+        this.coefficientQualite = coefficientQualite;
+    }
+
+    public double getCoutUnitaire() {
+        return coutUnitaire;
+    }
+    public void setCoutUnitaire(double coutUnitaire)
+    {
+        this.coutUnitaire = coutUnitaire;
+    }
+    public double getQuantite() {
+        return quantite;
+    }
+    public void setQuantite(double quantite){
+        this.quantite = quantite;
+    }
+
+    public double getCoefficientQualite() {
+        return coefficientQualite;
+    }
+
+    public void setCoefficientQualite(double coefficientQualite) {
+        this.coefficientQualite = coefficientQualite;
+    }
+
+    public double getCoutTransport() {
+        return coutTransport;
+    }
+
+    public void setCoutTransport(double coutTransport) {
+        this.coutTransport = coutTransport;
+    }
+}
