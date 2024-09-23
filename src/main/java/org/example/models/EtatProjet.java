d390a8afa36af6bbd7d2d605d5315e240cb37696
unknown <ismail.ouali@ump.ac.ma>
2024-09-23 11:57:33 +0100

    Fixing the enum error

---
+++ src/main/java/org/example/models/EtatProjet.java
@@ -0,0 +1,7 @@
+package org.example.models;
+
+public enum EtatProjet{
+    EN_COURS,
+    TERMINE,
+    ANNULE
+}
---
+++ src/main/java/org/example/models/Projet.java
@@ -0,0 +1,67 @@
+package org.example.models;
+
+
+import org.example.models.EtatProjet;
+
+public class Projet {
+    private int id;
+    private String nomProjet;
+    private double margeBeneficiaire;
+    private double coutTotal;
+    private EtatProjet etatProjet;
+    private int clientId;
+
+
+
+    public Projet(String nomProjet, double margeBeneficiaire, double coutTotal, EtatProjet etatProjet, int clientId) {
+        this.nomProjet = nomProjet;
+        this.margeBeneficiaire = margeBeneficiaire;
+        this.coutTotal = coutTotal;
+        this.etatProjet = etatProjet;
+        this.clientId = clientId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNomProjet() {
+        return nomProjet;
+    }
+
+    public void setNomProjet(String nomProjet) {
+        this.nomProjet = nomProjet;
+    }
+
+    public double getMargeBeneficiaire() {
+        return margeBeneficiaire;
+    }
+
+    public void setMargeBeneficiaire(double margeBeneficiaire) {
+        this.margeBeneficiaire = margeBeneficiaire;
+    }
+
+    public double getCoutTotal() {
+        return coutTotal;
+    }
+
+    public void setCoutTotal(double coutTotal) {
+        this.coutTotal = coutTotal;
+    }
+
+    public EtatProjet getEtatProjet() {
+        return etatProjet;
+    }
+
+    public void setEtatProjet(EtatProjet etatProjet) {
+        this.etatProjet = etatProjet;
+    }
+
+    public int getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(int clientId) {
+        this.clientId = clientId;
+    }
+}
